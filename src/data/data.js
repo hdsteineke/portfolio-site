1ce97011afcc0fcdb4b62c5f3c797d00cbf7dd7c
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-10-05 11:24:07 -0700

    added data file for project details

---
+++ src/data/data.js
@@ -0,0 +1,53 @@
+/* eslint-disable max-len */
+export const projects = [
+  {
+    id: '1',
+    name: 'TrashApp',
+    description: 'A social media app where users can upload images of items and crowd-source opinions on whether items pictured should be trashed or treasured via up/down voting. Completed over the course of a two-week sprint.',
+    contribution: '',
+    deployed: '',
+    github: '',
+    technologies: 'React | Cloudinary | Node.js | Material UI | Heroku | Netlify',
+    image: ''
+  },
+  {
+    id: '2',
+    name: 'Magica',
+    description: 'A deck-building app that allows users to draft, save and edit a collection of decks using an extensive Magic the Gathering API. This project was mob-programmed over the course of a 1-week sprint.',
+    contribution: '',
+    deployed: '',
+    github: '',
+    technologies: 'React | Supabase | Netlify',
+    image: ''
+  },
+  {
+    id: '3',
+    name: 'GiftWrApp',
+    description: 'A birthday-tracking app that allows users to save and edit birth dates, zodiac signs and gift ideas for their loved ones. This project was mob-programmed over the course of a 1-week sprint',
+    contribution: '',
+    deployed: '',
+    github: '',
+    technologies: 'Vanilla JS | Supabase',
+    image: ''
+  },
+  {
+    id: '4',
+    name: 'Alchemeetings the SQL',
+    description: 'A SQL-based backend built to provide a cleaner, more manageable user experience of the existing Alchemy Zoom classrooms. This app was built in a one-week sprint.',
+    contribution: '',
+    deployed: '',
+    github: '',
+    technologies: '',
+    image: ''
+  },
+  {
+    id: '5',
+    name: 'Portfolio Site',
+    description: '',
+    contribution: '',
+    deployed: '',
+    github: '',
+    technologies: '',
+    image: ''
+  },
+];
