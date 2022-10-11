45a5e6ec4d69cff255077790189eb5e731b56be9
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-10-11 12:26:16 -0700

    About Page styling and formatting

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,18 +5,16 @@ import styles from './About.css';
 
 export default function About() {
   return (
-    <div>
+    <div className={styles.AboutContainer}>
       <section className={styles.ImageContainer}>
         <img className={styles.Image} src="lionsmane.png" />
       </section>
       <section className={styles.About}>
-        <p>I am a people-oriented programmer who is passionate about co-creating healthy, balanced workspaces with an emphasis on open communication and mutual support. I entered the software industry out of a desire to weave my relational skills with my insatiable appetite for intellection and input. Whether it&apos;s solving puzzles, fixing bugs, or learning languages, I love a good challenge to chew on, and I am looking to join a team that values these skills!
+        <h1>Hailey Steineke <span className={styles.Pronouns}>(she/her)</span></h1>
+        <p>I am a <span className={styles.Highlight}>people-oriented</span> programmer who is passionate about co-creating workspaces that emphasize <span className={styles.Highlight}>open communication and mutual support</span>. I entered the software industry with the desire to weave my <span className={styles.Highlight}>relational skills</span> with my insatiable appetite for <span className={styles.Highlight}>problem-solving and pattern-recognition</span>. Whether it&apos;s solving puzzles, fixing bugs, or learning languages, I love a good challenge to chew on, and I am looking to join a team that values these skills!<br /><br />
         
         When I&apos;m not coding, you can find me playing with goats, reading the stars, and contemplating the nature of existence on this silly space marble. 
         </p>
-      </section>
-      <section className={styles.About}>
-        <p>Here&apos;s a little bit about my tech stack:</p>
       </section>
 
     </div>
--- src/components/Page/Layout.jsx
+++ src/components/Page/Layout.jsx
@@ -5,20 +5,17 @@ import './reset.css';
 import './global.css';
 import styles from './Layout.css';
 import FooterNav from './Footer/FooterNav.jsx';
-// import backgroundImg from '../../../public/starrynight.png';
 
 export default function Layout() {
   return (
     <>
       <div className={styles.Layout}>
-        {/* attempting to set background image with code below, but struggling to import background image */}
-        {/* <div className={styles.Layout} style={{ backgroundImg: `url(${backgroundImg})` }}>
-        </div> */}
+
         <Header />
         <main>
           <Outlet />
         </main>
-        <FooterNav />
+        {/* <FooterNav /> */}
       </div>
     </>
   );
