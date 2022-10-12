ccdb00e8342931e52fbc2fda3c93f941e8b89e87
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-10-12 11:39:52 -0700

    successfully wrapping project image and description when collapsing browser window

--- src/components/Portfolio/PortfolioItem.jsx
+++ src/components/Portfolio/PortfolioItem.jsx
@@ -4,11 +4,13 @@ import styles from './Portfolio.css';
 export default function PortfolioItem(project) {
   return (
     <div className={styles.ProjectContainer}>
-      <h1>{project.name}</h1>
+      <div className={styles.ProjectDescription}>
+        <h1><span className={styles.ProjectTitle}>{project.name}</span></h1>
+        <p>{project.github} | {project.deployed}</p>
+        <p><span id={styles.Technologies}>{project.technologies}</span></p><br/>
+        <p>{project.description}</p><br />
+      </div>
       <img className={styles.ProjectImage} src={project.image} />
-      <p>{project.technologies}</p><br/>
-      <p>{project.description}</p><br />
-      <p>{project.github} | {project.deployed}</p>
     </div>
   );
 }
