d014a764180e3b484f1550fdabb7eb91be628758
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-10-20 12:52:01 -0700

    added mui icons for github and linkedin to footer

--- src/components/Page/Footer/FooterNav.jsx
+++ src/components/Page/Footer/FooterNav.jsx
@@ -1,8 +1,12 @@
 import styles from '../Footer/FooterNav.css';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function FooterNav() {
   return (
-    <div className={styles.Footer}>This is a Footer!</div>
+    <div className={styles.Footer}>
+      <GitHubIcon />
+      <LinkedInIcon />
+    </div>
   );
 }
