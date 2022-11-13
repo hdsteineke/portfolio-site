9d78a68e6347149289f5053ce0725c0d38cea60b
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-11-13 11:22:02 -0800

    working nav icons to github and linkedin

--- src/components/Page/Footer/FooterNav.jsx
+++ src/components/Page/Footer/FooterNav.jsx
@@ -3,10 +3,18 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function FooterNav() {
+
+  function navToGitHub() {
+    window.open('https://github.com/hdsteineke');
+  }
+
+  function navToLinkedIn() {
+    window.open('https://www.linkedin.com/in/haileysteineke/');
+  }
   return (
     <div className={styles.Footer}>
-      <GitHubIcon className={styles.Icon} />
-      <LinkedInIcon className={styles.Icon}/>
+      <GitHubIcon onClick={navToGitHub} className={styles.Icon} />
+      <LinkedInIcon onClick={navToLinkedIn} className={styles.Icon}/>
     </div>
   );
 }
