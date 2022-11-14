f062cf5e08b122bb8213a071bff9b711d73213fd
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-11-14 10:37:48 -0800

    edited project descriptions

--- src/data/data.js
+++ src/data/data.js
@@ -3,18 +3,16 @@ export const projects = [
   {
     id: '1',
     name: 'TrashApp',
-    description: 'A social media app for users to crowd-source opinions about whether their possessions are trash or treasure. Users can upload images of items in a post, as well as up/down vote on other users posts, and even save their favorite posts to a "favorites" page. This project was developed remotely over the course of a two-week sprint.',
-    contribution: <a href="/portfolio/trashapp">Read more...</a>,
+    description: 'A full-stack social media app developed for users to crowd-source opinions about whether their possessions are trash or treasure. Users can upload images of items in a post, up/down vote on posts, and save posts to a "favorites" page. This project was built by a remote team over the course of a two-week sprint.',
     deployed: <a href="https://coruscating-duckanoo-7b598c.netlify.app/">Deployed</a>,
     github: <a href="https://github.com/Gift-WrApp-Crew">GitHub</a>,
-    technologies: 'React | Cloudinary | Node.js | Material UI | Heroku | Netlify',
+    technologies: 'React | SQL | Cloudinary | Node.js | Material UI | Heroku | Netlify',
     image: 'trashapp.PNG'
   },
   {
     id: '2',
     name: 'Magica',
-    description: 'A deck-building app that allows users to draft, save and edit a collection of decks using an extensive Magic the Gathering API. This project was mob-programmed over the course of a 1-week sprint.',
-    contribution: <a href="/portfolio/magica">Read more...</a>,
+    description: 'A dynamic full-stack CRUD app that allows users to build and customize Magic the Gathering decks using a 3rd party RESTful API. This project was developed by a remote team in a 1-week sprint.',
     deployed: <a href="https://endearing-paletas-f049d6.netlify.app/">Deployed</a>,
     github: <a href="https://github.com/Magica-Drafter/Magica">GitHub</a>,
     technologies: 'React | Supabase | Netlify',
@@ -23,8 +21,7 @@ export const projects = [
   {
     id: '3',
     name: 'GiftWrApp',
-    description: 'A birthday-tracking app that allows users to save and edit birth dates, zodiac signs and gift ideas for their loved ones. This project was mob-programmed over the course of a 1-week sprint',
-    contribution: <a href="/portfolio/giftwrapp">Read more...</a>,
+    description: 'A full-stack birthday-tracking app built for the desktop that allows users to save and edit birth dates, zodiac signs and gift ideas for their loved ones. This project was developed by a remote team in a 1-week sprint',
     deployed: 'Deployed',
     github: <a href="https://github.com/delaneyfogarty/Gift-wrApp">Github</a>,
     technologies: 'Vanilla JS | Supabase',
@@ -33,21 +30,10 @@ export const projects = [
   {
     id: '4',
     name: 'Alchemeetings the SQL',
-    description: 'A SQL-based backend built to provide a cleaner, more manageable user experience of the existing Alchemy Zoom classrooms. This app was built in a one-week sprint.',
-    contribution: <a href="/portfolio/alchemeetings">Read more...</a>,
+    description: 'A backend built with SQL to provide a cleaner, more manageable user experience of the existing Alchemy Zoom classrooms. This app implements GitHub OAuth for user authentication and was developed remotely in a one-week sprint.',
     deployed: <a href="https://alchemeetings-backend.herokuapp.com/">Deployed</a>,
     github: <a href="https://github.com/Alchemy-Meeting-Backend/alchemy-meetings-backend">GitHub</a>,
     technologies: 'SQL | GitHub OAuth | Jest | Express | Node.js',
     image: 'alchemeetingSQL.png'
-  },
-  {
-    id: '5',
-    name: 'Portfolio Site',
-    description: '',
-    contribution: <a href="/portfolio/portfolio">Read more...</a>,
-    deployed: <a href="https://haileysteineke.netlify.app/">Deployed</a>,
-    github: <a href="https://github.com/hdsteineke/portfolio-site">GitHub</a>,
-    technologies: '',
-    image: ''
-  },
+  }
 ];
