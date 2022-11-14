93b53a3beaacd91fc80d24321ff5c2d5fe16e4c0
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-11-14 10:37:04 -0800

    file cleanup

--- src/components/Home/Home.jsx
+++
@@ -1,9 +0,0 @@
-import styles from  './Home.css';
-
-export default function Home() {
-  return (
-    <div className={styles.Home}>
-      <p>There&apos;s no place like Home.</p>
-    </div>
-  );
-}
