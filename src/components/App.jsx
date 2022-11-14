0e92dbf8184a5967d9e8b5eac8232e20e8139986
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-11-14 09:53:42 -0800

    cleaning up unused components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-
 import {
   BrowserRouter as Router,
   Routes,
@@ -6,12 +5,7 @@ import {
   Navigate,
 } from 'react-router-dom';
 import Layout from './Page/Layout.jsx';
-import Home from './Home/Home.jsx';
 import PortfolioList from './Portfolio/PortfolioList.jsx';
-import Trashapp from './Portfolio/Trashapp.jsx';
-import Magica from './Portfolio/Magica.jsx';
-import Giftwrapp from './Portfolio/Giftwrapp.jsx';
-import Alchemeetings from './Portfolio/Alchemeetings.jsx';
 import About from './About/About.jsx';
 import Resume from './Resume/Resume.jsx';
 
@@ -22,13 +16,8 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="portfolio" element={<PortfolioList />} />
-            <Route path="portfolio/trashapp" element={<Trashapp/>} />
-            <Route path="portfolio/magica" element={<Magica />} />
-            <Route path="portfolio/giftwrapp" element={<Giftwrapp />} />
-            <Route path="portfolio/alchemeetings" element={<Alchemeetings />} />
             <Route path="/" element={<About />} />
             <Route path="resume" element={<Resume />} />
-            {/* <Route index element={<Home />} /> */}
             <Route path="*" element={<Navigate to="/" replace />} />
           </Route>
         </Routes>
