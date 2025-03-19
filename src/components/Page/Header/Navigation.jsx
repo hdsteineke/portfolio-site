198183b0fe44c26a14e2043ac8b20ef4d555827d
HAILEY STEINEKE <hailey.steineke@gmail.com>
2025-03-19 16:01:54 -0700

    added updated resume to navbar

--- src/components/Page/Header/Navigation.jsx
+++ src/components/Page/Header/Navigation.jsx
@@ -6,7 +6,7 @@ export default function Navigation() {
     <nav className={styles.Navigation}>
       <NavLink to="portfolio">Portfolio</NavLink>
       <NavLink to="about">About</NavLink>
-      <NavLink target={'_blank'} to="resume.pdf">Resume</NavLink>
+      <NavLink target={'_blank'} to="techresume2024.pdf">Resume</NavLink>
     </nav>
   );
 }
--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,5 +1,5 @@
 export default function Resume() {
   return (
-    <a target="_blank" href="resume.pdf">Resume</a>
+    <a target="_blank" href="techresume2024.pdf">Resume</a>
   );
 }
