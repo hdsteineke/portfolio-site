2e6171f5ad127bfa234645040569af6e124b4c88
HAILEY STEINEKE <hailey.steineke@gmail.com>
2025-11-25 11:55:24 -0800

    updated About Me

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,11 +15,11 @@ export default function About() {
       </section>
       <section className={styles.About}>
         <h1>Hailey Steineke <span className={styles.Pronouns}>(she/her)</span></h1>
-        <p>I have many interests, of which coding is just one. What really draws me in is language: the power and meaning behind a set of words or commands. The symbolism and patterns used to convey those meanings, and all the creative potential unlocked when one is able to speak a respective language. I see it in programming, spoken word, music, astrology, just to name a few.
+        <p>I have many interests, of which coding is just one. What really draws me in is <span className={styles.Highlight}>language:</span> the power and meaning behind a set of words or commands, the symbolism and patterns used to convey those meanings, and all <span className={styles.Highlight}>the creative potential unlocked when one is able to speak a respective language.</span> I see it in programming, spoken word, music, astrology, just to name a few.
           <br></br>
           <br></br>
           
-        What brings me here is my desire to use the language of programming to create tools that inspire and support others on their individual journeys through healthy, creative living, as well as express my own creativity.<span className={styles.Highlight}>&apos;</span>.
+        What brings me here is my desire to use the language of programming to create <span className={styles.Highlight}>tools that inspire and support others on their individual journeys through healthy, creative living,</span> as well as express my own creativity.<span className={styles.Highlight}></span>
           <br></br>
           <br></br>
         
